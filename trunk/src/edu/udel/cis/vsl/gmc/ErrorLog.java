4a0802c881f500253fccfa0b127f3ef91636e80d
siegel <siegel@6e9fb2d9-15d7-6d4b-b522-be9514ac623a>
2013-11-27 20:55:44 +0000

    Documenting GMC.

--- trunk/src/edu/udel/cis/vsl/gmc/ErrorLog.java
+++ trunk/src/edu/udel/cis/vsl/gmc/ErrorLog.java
@@ -12,20 +12,45 @@ import java.util.TreeMap;
  * A log for recording errors and corresponding traces encountered during a
  * model checking run.
  * 
- * @author siegel
+ * In the course of performing a search, various kinds of "errors" may be
+ * encountered. These do not necessarily pause the search. They are not even
+ * necessarily noticed by the searcher. Each application using this package is
+ * free to determine what in an error is and how to report them. Typically, an
+ * error occurs in the process of computing the enabled transitions at a state,
+ * or in the process of executing a transition. Division by zero, null pointer
+ * dereferences, and so on, are typical errors that should be logged and
+ * reported.
+ * 
+ * This class provides a convenient mechanism for logging such as errors as the
+ * search progresses. Many applications do not necessarily stop after the first
+ * error is discovered, but instead take some corrective mechanism (like
+ * constraining the path condition in symbolic execution) and proceed with the
+ * search. Those applications can use this class to record the errors
+ * encountered.
+ * 
+ * In addition to recording errors, this class provides a number of convenient
+ * services: using information provided by the application, it can determine
+ * when two errors are considered "equivalent" and therfore only one instance
+ * needs to be reported; it can prioritize the errors so that the most important
+ * are reported to the user first; it can associate a trace to each error and
+ * save these in a file, so the user can later replay the trace associate to
+ * each error discovered.
+ * 
+ * @author Stephen F. Siegel, University of Delaware
  * 
  */
 public class ErrorLog {
 
-	// TODO: need to add error bound and throw of excessive error exception
-
 	// Instance fields...
 
+	/**
+	 * The searcher performing the search.
+	 */
 	private DfsSearcher<?, ?, ?> searcher;
 
 	/**
-	 * Map used for storing the reported exceptions. The value is the length of
-	 * the shortest trace encountered for that exception.
+	 * A canonical map used for storing and flyweighting the reported errors.
+	 * Key=value for all entries in this map.
 	 */
 	private SortedMap<LogEntry, LogEntry> entryMap;
 
@@ -35,7 +60,7 @@ public class ErrorLog {
 	private File directory;
 
 	/**
-	 * Names of the session: this name will be used to form the file name of all
+	 * Name of the session: this name will be used to form the file name of all
 	 * files created by this log.
 	 */
 	private String sessionName;
@@ -53,20 +78,41 @@ public class ErrorLog {
 	private int numErrors = 0;
 
 	/**
-	 * Total number of errors that can be reported before this log thrown an
-	 * ExcessiveErrorException;
+	 * Total number of errors that can be reported before this log throws an
+	 * {@link ExcessiveErrorException}.
 	 */
 	private int errorBound = 5;
 
+	/**
+	 * Was this search truncated due to an excessive error exception?
+	 */
 	private boolean searchTruncated = false;
 
+	/**
+	 * The stream to which errors should be printed when they are logged.
+	 * Typical applications want to report errors as soon as they are
+	 * discovered, but continue with the search, and then also save the log of
+	 * these errors.
+	 */
 	private PrintStream out;
 
+	/**
+	 * The file to which this log will be printed (in human-readable form) at
+	 * the end.
+	 */
 	private File logFile;
 
 	/**
-	 * Creates new ErrorLog with given comparator
+	 * Creates new ErrorLog.
 	 * 
+	 * @param directory
+	 *            the directory in which traces and the log file will be stored
+	 * @param sessionName
+	 *            the name to use for this session; it will form the root of the
+	 *            names of all the files created
+	 * @param out
+	 *            the stream to which errors should be printed when they are
+	 *            reported to this log
 	 */
 	public ErrorLog(File directory, String sessionName, PrintStream out) {
 		this.out = out;
@@ -87,16 +133,40 @@ public class ErrorLog {
 
 	// Helper methods...
 
+	/**
+	 * The name to use for the trace file, for the error of ID i reported to
+	 * this log.
+	 * 
+	 * @param i
+	 *            an integer
+	 * @return a file name formed from the seesion name and the number i
+	 */
 	private String traceFileName(int i) {
 		return sessionName + "_" + i + ".trace";
 	}
 
+	/**
+	 * Returns the java File object corresponding to a file with name
+	 * traceFileName(i) in the log directory. This does not create such a file,
+	 * it merely constructs and returns the Java platform-independent
+	 * representation of the file name as a File object.
+	 * 
+	 * @param i
+	 *            an integer
+	 * @return a File in the directory whose name is formed from the session
+	 *         name and the number i
+	 */
 	private File traceFile(int i) {
 		return new File(directory, traceFileName(i));
 	}
 
 	// Public methods...
 
+	/**
+	 * Returns the directory in which trace files and the log will be stored.
+	 * 
+	 * @return the directory associated to this log
+	 */
 	public File getDirectory() {
 		return directory;
 	}
@@ -163,7 +233,7 @@ public class ErrorLog {
 		if (oldEntry != null) {
 			int id = oldEntry.getId();
 			File file = oldEntry.getTraceFile();
-			int oldLength = oldEntry.getSize();
+			int oldLength = oldEntry.getLength();
 
 			out.println("New log entry is equivalent to previously encountered entry "
 					+ id);
